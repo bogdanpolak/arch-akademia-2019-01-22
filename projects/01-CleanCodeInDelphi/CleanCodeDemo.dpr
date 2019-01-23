a9170cef4255b07e835465d81e21673c9717e363
Szkoleniowy Jeden <komp.szkoleniowy1@bsc.com.pl>
2019-01-23 12:32:13 +0100

    Dodany Helper.TDBGrid

--- projects/01-CleanCodeInDelphi/CleanCodeDemo.dpr
+++ projects/01-CleanCodeInDelphi/CleanCodeDemo.dpr
@@ -18,7 +18,8 @@ uses
   DataAccess.Books.FireDAC in 'experimental\DataAccess.Books.FireDAC.pas',
   ClientAPI.Books in 'api\ClientAPI.Books.pas',
   ClientAPI.Readers in 'api\ClientAPI.Readers.pas',
-  ExtGUI.ListBox.Books in 'ExtGUI.ListBox.Books.pas';
+  ExtGUI.ListBox.Books in 'ExtGUI.ListBox.Books.pas',
+  Helper.TDBGrid in 'Helper.TDBGrid.pas';
 
 {$R *.res}
 
